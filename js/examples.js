cb2b5011b49f0fa9456ae1072dd07c9a09b534e9
Mikael Jergefelt <mikael.jergefelt@ki.se>
2016-10-11 22:42:50 +0200

    add example using aria-expanded, aria-hidden and nav

--- js/examples.js
+++ js/examples.js
@@ -17,13 +17,66 @@
   };
 
   Vue.component('accessibility-tree-fail', {
-    'template': '<div class="button" v-bind:class="{ clicked: clicked }" v-on:click="click">Click me</div>',
+    'template': `
+      <div class="button" v-bind:class="{ clicked: clicked }" v-on:click="click">Click me</div>
+    `,
     'mixins': [buttonClick]
   });
 
   Vue.component('accessibility-tree-win', {
-    'template': '<button v-bind:class="{ clicked: clicked }" v-on:click="click">Click me</button>',
+    'template': `
+      <button v-bind:class="{ clicked: clicked }" v-on:click="click">Click me</button>
+    `,
     'mixins': [buttonClick]
+  });
+
+  Vue.component('menu-fail', {
+    'template': `
+      <div class="navigation">
+        <button v-on:click="hidden = !hidden">Menu</button>
+        <div class="menu" v-bind:class="{ hidden: hidden }">
+          <a href="#">First link</a> <a href="#">Second link</a> <a href="#">Third link</a>
+        </div>
+      </div>
+    `,
+    'data': function data() {
+      return {
+        'hidden': true
+      };
+    }
+  });
+
+  Vue.component('menu-win', {
+    'template': `
+      <nav>
+        <button v-bind:aria-expanded="ariaExpanded" v-on:click="hidden = !hidden">Menu</button>
+        <ul v-bind:aria-hidden="ariaHidden">
+          <li>
+            <a href="#">First link</a>
+          </li>
+          <li>
+            <a href="#">Second link</a>
+          </li>
+          <li>
+            <a href="#">Third link</a>
+          </li>
+        </ul>
+      </nav>
+    `,
+    'data': function data() {
+      return {
+        'hidden': true
+      };
+    },
+    'computed': {
+      // return aria-values as string so false ones won't be removed
+      'ariaExpanded': function ae() {
+        return this.hidden ? 'false' : 'true';
+      },
+      'ariaHidden': function ah() {
+        return this.hidden ? 'true' : 'false';
+      }
+    }
   });
 
   window.example = new Vue({
